2921f615ce9770cee0fa50ed0379434b9dae4c6c
agurka <adrian.gurka@gmail.com>
2025-02-22 20:53:17 +0100

    implement syncing, job management

--- jobs.py
+++ jobs.py
@@ -1,18 +1,18 @@
 import utils
 import json
 import os
+import logging
 from pprint import pprint
 
 
 def list_specific_job(jobname):
-    job_dir = utils.get_job_directory()
-    with open(os.path.join(job_dir, f"{jobname}.json")) as f:
-        job = json.load(f)
+    job = utils.get_job_by_name(jobname)
+    print(f"{jobname}:")
     pprint(job)
 
 
 def list_all_jobs():
-    job_dir = utils.get_job_directory()
+    job_dir = utils.get_or_create_job_directory()
 
     jobs = os.listdir(job_dir)
     jobs = [os.path.splitext(job)[0] for job in jobs]
@@ -27,7 +27,50 @@ def list_all_jobs():
 
 
 def list_jobs(args):
-    if args.name:
-        list_specific_job(args.name)
+    if args["name"]:
+        list_specific_job(args["name"])
     else:
         list_all_jobs()
+
+
+def remove_job(args):
+    job_name = args.get("name")
+    assert job_name, "must provide job name if 'remove' is true"
+
+    job_filepath = os.path.join(utils.get_or_create_job_directory(), f"{job_name}.json")
+
+    logging.info(f"Removing job {job_filepath}")
+    os.remove(job_filepath)
+
+
+def add_job(args):
+    job_dir = utils.get_or_create_job_directory()
+
+    job_name = args["job"]
+    job_filename = f"{job_name}.json"
+    job_filepath = os.path.join(job_dir, job_filename)
+
+    src = os.path.abspath(args["src"])
+    dest = os.path.abspath(args["dest"])
+    log = args["logfile"]
+
+    if job_filename in os.listdir(job_dir):
+        print(
+            f"Job '{job_name}' already exists. Create new name or remove '{job_filepath}'"
+        )
+        return
+
+    job = {"src": src, "dest": dest, "logfile": log, "cron": None}
+    utils.set_logging(args, job)
+    with open(job_filepath, "w") as jf:
+        logging.info(f"created job {job_name} at {job_filepath}")
+        json.dump(job, jf, sort_keys=True, indent=4)
+
+
+def run(args):
+    job = utils.get_job_by_name(args["job"])
+
+    utils.set_logging(args, job)
+    logging.info("Starting sync job")
+
+    utils.sync_directories(job["src"], job["dest"])
--- utils.py
+++ utils.py
@@ -1,6 +1,33 @@
 import os
 import sys
 import logging
+import json
+import shutil
+import filecmp
+import argparse
+
+
+def set_logging(args, job):
+    logging_setup = {
+        "level": get_log_levels().get(args["verbosity"]),
+        "format": "%(asctime)s - %(levelname)s - %(message)s",
+        "datefmt": "%Y-%m-%d %H:%M:%S",
+    }
+
+    logfile = job.get("logfile")
+    if args["command"] == "setup":
+        if logfile:
+            logging_setup["filename"] = logfile
+        else:
+            print(
+                "No logfile provided on job creation. Logs for this job will not be stored to a file"
+            )
+
+    if args["command"] == "run":
+        if logfile:
+            logging_setup["filename"] = logfile
+
+    logging.basicConfig(**logging_setup)
 
 
 def get_log_levels():
@@ -13,7 +40,48 @@ def get_log_levels():
     }
 
 
-def get_job_directory():
+def parse_args():
+    desc = "dirsync performs one-way syncing of 2 directories"
+    parser = argparse.ArgumentParser(description=desc)
+
+    default_log_level = "warn"
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        choices=get_log_levels().keys(),
+        default=default_log_level,
+        help=f"Set verbosity level. Defaults to {default_log_level}.",
+    )
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    setup_parser = subparsers.add_parser("setup", help="setup and configure a sync job")
+    setup_parser.add_argument("job", help="job name")
+    setup_parser.add_argument("src", help="source directory")
+    setup_parser.add_argument("dest", help="destination directory")
+    setup_parser.add_argument(
+        "-l",
+        "--logfile",
+        help="path to log file. If not provided, no logs will be stored.",
+        required=False,
+        default=False,
+    )
+
+    run_parser = subparsers.add_parser("run", help="run a sync job")
+    run_parser.add_argument("job", help="name of the job to run")
+
+    jobs_parser = subparsers.add_parser("jobs", help="list jobs")
+    jobs_parser.add_argument(
+        "-r", "--remove", help="remove job by name", action="store_true"
+    )
+    jobs_parser.add_argument("name", nargs="?", help="list specific job")
+
+    args = parser.parse_args()
+
+    return args.__dict__
+
+
+def get_or_create_job_directory():
     if sys.platform == "win32":
         path = os.getenv("APPDATA", "%APPDATA%")
 
@@ -22,8 +90,85 @@ def get_job_directory():
 
     job_dir = os.path.join(path, "dirsync", "jobs")
     if not os.path.exists(job_dir):
-
         logging.info(f"Job directory not found. Creating {job_dir}")
         os.makedirs(job_dir)
 
     return job_dir
+
+
+def get_job_by_name(jobname):
+    job_dir = get_or_create_job_directory()
+    job_filename = f"{jobname}.json"
+
+    all_jobs = os.listdir(job_dir)
+    assert job_filename in all_jobs, f"job '{jobname}' does not exist"
+
+    with open(f"{os.path.join(job_dir, job_filename)}") as jf:
+        return json.load(jf)
+
+
+def compare_trees(src_root, dest_root):
+    """
+    Function compares 2 trees, returning a dict with keys 'remove' and 'overwrite'.
+    The values are lists with files that need removing or simply overwritten with src file
+    """
+    to_remove = []
+    to_write = []
+
+    source_files = {}
+    dest_files = {}
+
+    for root, _, files in os.walk(src_root):
+        for file in files:
+            relative_path = os.path.relpath(os.path.join(root, file), src_root)
+            source_files[relative_path] = os.path.join(root, file)
+
+    for root, _, files in os.walk(dest_root):
+        for file in files:
+            relative_path = os.path.relpath(os.path.join(root, file), dest_root)
+            dest_files[relative_path] = os.path.join(root, file)
+
+    for relative_path in dest_files:
+        if relative_path not in source_files:
+            to_remove.append(relative_path)
+
+    for relative_path, source_file in source_files.items():
+        if relative_path in dest_files:
+            dest_file = dest_files[relative_path]
+            if not filecmp.cmp(source_file, dest_file, shallow=False):
+                to_write.append(relative_path)
+        else:
+            to_write.append(relative_path)
+
+    return {"remove": to_remove, "write": to_write}
+
+
+def copy_files(src, dest, files):
+    for file in files:
+        source_path = os.path.join(src, file)
+        dest_path = os.path.join(dest, file)
+
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+        logging.debug(f"Writing file {source_path} to destination folder.")
+        shutil.copy(source_path, dest_path)
+
+
+def remove_files(dest, files):
+    for file in files:
+        path = os.path.join(dest, file)
+        if os.path.isfile(path):
+            logging.debug(f"Removing file {path}")
+            os.remove(path)
+        else:
+            logging.debug(f"Removing directory {path}")
+            shutil.rmtree(path)
+
+
+def sync_directories(src, dest):
+    diffs = compare_trees(src, dest)
+
+    remove_files(dest, diffs["remove"])
+    copy_files(src, dest, diffs["write"])
+
+    logging.info(f"Directory {dest} is synced to {src}")
